7712bfbfc7fbb6b2ac2344053e6fa8d87083bd93
Yoonsangbyung <ysb1706@naver.com>
2024-04-14 22:19:52 +0900

    컴퓨터 프로그래밍 6주차 퀴즈

---
+++ test6.py
@@ -0,0 +1,24 @@
+fruits = ["사과", "바나나", "딸기"]
+for fruit in fruits:
+    print(fruit)
+
+dic = {"과일": "사과", '숫자' : 1, '사람 이름': '홍길동'}
+for i in dic :
+    print(i, dic[i])
+
+for k in range(0, 10):
+    print(k)
+
+content = "사과 같은 배"
+while content == "사과":
+    print("사과입니다.")
+else:
+    print("사과가 아닌 다른 것이므로 코드 실행을 중지합니다.")
+
+number = 1
+while number < 5 :
+    print(number)
+    number = number + 1
+
+else :
+    print("number 변수가 5 이상이면 종료합니다.")
